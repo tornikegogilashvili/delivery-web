83a93c5b4b80e5dfa2bd90f992c9c8b738707df9
tornike gogilashvili <tornikegogilashvili@yahoo.com>
2022-11-17 23:49:28 +0400

    changes

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@
 function change(){
     console.log(document.getElementById("element").innerHTML); 
     if(document.getElementById("element").innerHTML=='<i class="fa-solid fa-toggle-off" onclick="change()"></i>'){
-        document.getElementById("element").innerHTML='<i class="fa-solid fa-toggle-on" onclick="change()"></i>'
+        document.getElementById("element").innerHTML='<i class="fa-solid fa-toggle-on" onclick="change()"></i>';
         document.getElementById("body").style.backgroundColor="#000000";
         document.getElementById("logo_a").style.color="#F5F5F5";
-        document.getElementById("element").style.color="#ffffff";
+        document.getElementById("element").style.color="#F5F5F5";
         document.getElementById("nav_ul_li_a1").style.color="#F5F5F5";
         document.getElementById("nav_ul_li_a2").style.color="#F5F5F5";
         document.getElementById("nav_ul_li_a3").style.color="#F5F5F5";
@@ -53,15 +53,27 @@ function change1(){
 
 }
 //----------services.html
-function change2() {
+function change2(){
     console.log(document.getElementById("element2").innerHTML);
     if(document.getElementById("element2").innerHTML=='<i class="fa-solid fa-toggle-off" onclick="change2()"></i>'){
         document.getElementById("element2").innerHTML='<i class="fa-solid fa-toggle-on" onclick="change2()"></i>';
         document.getElementById("body2").style.backgroundColor="#F5F5F5";
         document.getElementById("logo_a2").style.color="#000000";
+        document.getElementById("nav_ul_li_a1_11").style.color="#000000";
+        document.getElementById("nav_ul_li_a2_22").style.color="#000000";
+        document.getElementById("nav_ul_li_a3_33").style.color="#000000";
+        document.getElementById("nav_ul_li_a4_44").style.color="#000000";
+        document.getElementById("section_satauri11").style.color="#000000";
+        document.getElementById("element2").style.color="#000000";
     }else{
         document.getElementById("element2").innerHTML='<i class="fa-solid fa-toggle-off" onclick="change2()"></i>';
         document.getElementById("body2").style.backgroundColor="#000000";
         document.getElementById("logo_a2").style.color="#F5F5F5";
+        document.getElementById("nav_ul_li_a1_11").style.color="#F5F5F5";
+        document.getElementById("nav_ul_li_a2_22").style.color="#F5F5F5";
+        document.getElementById("nav_ul_li_a3_33").style.color="#F5F5F5";
+        document.getElementById("nav_ul_li_a4_44").style.color="#F5F5F5";
+        document.getElementById("section_satauri11").style.color="#F5F5F5";
+        document.getElementById("element2").style.color="#F5F5F5";
     }
 }
